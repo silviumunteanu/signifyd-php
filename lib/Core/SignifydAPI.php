910aaa001a37ebb64b77bec07b80346847964fa1
Cristian <cristian.vatca@osf-global.com>
2017-05-16 12:51:04 +0300

    Improve loging version
    Adding monolog and start logging in the extension.

--- lib/Core/SignifydAPI.php
+++ lib/Core/SignifydAPI.php
@@ -6,6 +6,9 @@
  */
 namespace Signifyd\Core;
 
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
 class SignifydAPI
 {
     /**
@@ -13,33 +16,44 @@ class SignifydAPI
      */
     private $settings;
 
+    public $logger;
+
     private function logError($message)
     {
-        if ($this->settings->logErrors && $this->settings->loggerError) {
-            call_user_func($this->settings->loggerError, $message);
+        if($this->settings->logErrors && $this->settings->loggerError)
+        {
+//            call_user_func([$this->logger, $this->settings->loggerError], $message);
+            $this->logger->error($message);
         }
     }
 
     private function logWarning($message)
     {
-        if ($this->settings->logWarnings && $this->settings->loggerWarning) {
-            call_user_func($this->settings->loggerWarning, $message);
+        if($this->settings->logWarnings && $this->settings->loggerWarning)
+        {
+//            call_user_func([$this->logger, $this->settings->loggerWarning], $message);
+            $this->logger->warning($message);
         }
     }
 
     private function logInfo($message)
     {
-        if ($this->settings->logInfo && $this->settings->loggerInfo) {
-            call_user_func($this->settings->loggerInfo, $message);
+        if($this->settings->logInfo && $this->settings->loggerInfo)
+        {
+//            call_user_func([$this->logger, $this->settings->loggerInfo], $message);
+            $this->logger->info($message);
         }
     }
 
     public function __construct(SignifydSettings $settings)
     {
-        if (is_null($settings->apiKey)) {
+        if(is_null($settings->apiKey))
+        {
             throw new \Exception("API key is required.");
         }
         $this->settings = $settings;
+        $this->logger = new Logger('Signifyd PHP Library');
+        $this->logger->pushHandler(new StreamHandler($this->settings->logFileLocation . '/' . $this->settings->logFileName));
     }
 
     private function makeUrl($endpoint)
@@ -74,6 +88,8 @@ class SignifydAPI
         $curl = $this->_setupPostJsonRequest($this->makeUrl("cases"), $case);
         $response = curl_exec($curl);
         $info = curl_getinfo($curl);
+        $this->logInfo("Raw Request: " . json_encode($info));
+
         $error = curl_error($curl);
         curl_close($curl);
 
@@ -154,6 +170,8 @@ class SignifydAPI
     public function validWebhookRequest($request, $hash, $topic)
     {
         $check = base64_encode(hash_hmac('sha256', $request, $this->settings->apiKey, true));
+        $this->logInfo("Api request hash: " . $hash);
+        $this->logInfo("Api request hash check: " . $check);
 
         if ($check == $hash) {
             return true;
@@ -202,6 +220,7 @@ class SignifydAPI
     private function _setupPostRequest($url, $postBody, $contentType)
     {
         $curl = $this->_setupRequestCommon($url);
+        $this->logInfo("Request post body: " . $postBody);
 
         $headers = array();
         $headers[] = "Accept: application/json";
@@ -218,6 +237,7 @@ class SignifydAPI
     private function _setupPutRequest($url, $postBody, $contentType)
     {
         $curl = $this->_setupRequestCommon($url);
+        $this->logInfo("Request put body: " . $postBody);
 
         $headers = array();
         $headers[] = "Accept: application/json";
